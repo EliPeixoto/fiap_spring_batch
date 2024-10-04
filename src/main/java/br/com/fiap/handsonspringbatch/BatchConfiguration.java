effd59b9f3fb34e213fd66727c3fd489092ad3e3
EliPeixoto <eliane.peixoto@outlook.com>
2024-10-03 21:44:13 -0300

    finalizado aprendizado batch

--- src/main/java/br/com/fiap/handsonspringbatch/BatchConfiguration.java
+++ src/main/java/br/com/fiap/handsonspringbatch/BatchConfiguration.java
@@ -2,27 +2,82 @@ package br.com.fiap.handsonspringbatch;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class BatchConfiguration {
-    public Job processarPerson(){
-        return null;
+    private final JobRepository jobRepository;
+
+    public BatchConfiguration(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
     }
 
-    public Step step(){
-        return null;
+    @Bean
+    public Job processarPerson(JobRepository jobRepository, Step step) {
+        return new JobBuilder("processarPerson", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(step)
+                .build();
     }
 
-    public ItemReader<Person> itemReader(){
-        return null;
+    @Bean
+    public Step step(JobRepository jobRepository,
+                     PlatformTransactionManager platformTransactionManager,
+                     ItemReader<Person> itemReader,
+                     ItemWriter<Person> itemWriter,
+                     ItemProcessor<Person, Person> itemProcessor
+                        ) {
+        return new StepBuilder("step", jobRepository)
+                .<Person, Person>chunk(20, platformTransactionManager)
+                .reader(itemReader)
+                .processor(itemProcessor)
+                .writer(itemWriter)
+                .build();
     }
 
-    public ItemWriter<Person> itemWriter(){
-        return null;
+    @Bean
+    public ItemReader<Person> itemReader() {
+        BeanWrapperFieldSetMapper<Person> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(Person.class);
+        return new FlatFileItemReaderBuilder<Person>()
+                .name("personItemReader")
+                .resource(new ClassPathResource("people.csv"))
+                .delimited()
+                .names("name", "streetName", "number", "city", "country", "email", "phoneNumber")
+                .fieldSetMapper(fieldSetMapper)
+                .build();
     }
 
+    @Bean
+    public ItemWriter<Person> itemWriter(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<Person>()
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .dataSource(dataSource)
+                .sql("INSERT INTO person"
+                + "(nome, street_name, number, city, country, email, phoneNumber, create_date_time)"
+                +"VALUES(:name, :streetName, :number, :city, :country, :email, :phoneNumber, :createDateTime)")
+                .build();
+    }
+
+    @Bean
+    public ItemProcessor<Person, Person> itemProcessor(){
+        return new PersonProcessor();
+    }
 
 }
